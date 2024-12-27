b08f03f51fa162f1581a70620c70d365cc853715
Nezhdanchik1 <myktybekov1234@gmail.com>
2024-12-27 13:57:42 +0500

    Мелкие изменения

---
+++ src/main/java/org/example/fitnessplatform/dto/LoginDTO.java
@@ -0,0 +1,12 @@
+package org.example.fitnessplatform.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class LoginDTO {
+    private String email;
+    private String password;
+
+}
