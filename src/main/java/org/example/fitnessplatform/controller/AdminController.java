b499ba4fea0b8d8887c7502d84bc12614dd1c3c6
Nezhdanchik1 <myktybekov1234@gmail.com>
2024-12-28 01:40:59 +0500

    Мелкие изменения

---
+++ src/main/java/org/example/fitnessplatform/controller/AdminController.java
@@ -0,0 +1,26 @@
+package org.example.fitnessplatform.controller;
+
+import org.example.fitnessplatform.dto.UserDto;
+import org.example.fitnessplatform.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/admin")
+public class AdminController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/users")
+    @PreAuthorize("hasAnyAuthority('ADMIN')")
+    public List<UserDto> getAllUsers() {
+        return userService.getAllUsers();
+    }
+}
--- src/main/java/org/example/fitnessplatform/controller/UserController.java
+++ src/main/java/org/example/fitnessplatform/controller/UserController.java
@@ -5,7 +5,9 @@ import org.example.fitnessplatform.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -15,16 +17,19 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    @GetMapping
-    @PreAuthorize("hasAnyAuthority('ADMIN')")
-    public List<UserDto> getAllUsers() {
-        return userService.getAllUsers();
-    }
-
     @GetMapping("/{userId}")
     public UserDto getUserById(@PathVariable Long userId) {
         return userService.getUserById(userId);
     }
+
+    @PutMapping("/updateUser")
+    public String updateUser(@RequestParam(value = "name", required = false) String newName,
+                              @RequestParam(value = "avatar", required = false) MultipartFile newAvatar,
+                              @RequestParam(value = "old_password", required = false) String oldPassword,
+                              @RequestParam(value = "new_password", required = false) String newPassword) throws IOException {
+        return userService.updateUser(newName, newAvatar, oldPassword, newPassword);
+    }
+
     @DeleteMapping("/{userId}")
     @PreAuthorize("hasAnyAuthority('ADMIN')")
     public void deleteUser(@PathVariable Long userId) {
