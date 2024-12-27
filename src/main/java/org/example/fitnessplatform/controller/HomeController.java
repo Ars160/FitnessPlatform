3e844f166e9626b87ef61ecd5c57fc563722593d
Nezhdanchik1 <myktybekov1234@gmail.com>
2024-12-27 18:55:07 +0500

    Мелкие изменения

--- src/main/java/org/example/fitnessplatform/controller/HomeController.java
+++ src/main/java/org/example/fitnessplatform/controller/HomeController.java
@@ -14,7 +14,7 @@ public class HomeController {
     }
 
     @GetMapping("/register_page")
-    public String showRegiterPage(){
+    public String showRegisterPage(){
         return "register_page";
     }
 
