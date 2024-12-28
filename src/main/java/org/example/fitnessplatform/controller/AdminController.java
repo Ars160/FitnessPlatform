f3fe7d624b5050fa73a78c1375534f93b6e188f4
Nezhdanchik1 <myktybekov1234@gmail.com>
2024-12-28 05:42:18 +0500

    Мелкие изменения

--- src/main/java/org/example/fitnessplatform/controller/AdminController.java
+++ src/main/java/org/example/fitnessplatform/controller/AdminController.java
@@ -3,13 +3,14 @@ package org.example.fitnessplatform.controller;
 import org.example.fitnessplatform.dto.UserDto;
 import org.example.fitnessplatform.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/admin")
@@ -22,5 +23,13 @@ public class AdminController {
     @PreAuthorize("hasAnyAuthority('ADMIN')")
     public List<UserDto> getAllUsers() {
         return userService.getAllUsers();
+    }
+
+    @PostMapping("/api/admin/users/updateRole")
+    public ResponseEntity<Map<String, Object>> updateUserRole(@RequestParam Long id, @RequestParam String role) {
+        userService.updateUserRole(id, role);
+
+        Map<String, Object> response = new HashMap<>();
+        return ResponseEntity.ok(response);
     }
 }
